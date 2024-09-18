e6b2b2df11f622bfa43f735f82faad5eb48cda22
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2024-09-18 21:20:26 +0600

    home added

--- JS/login.js
+++ JS/login.js
@@ -1,7 +1,13 @@
 document.getElementById('button-login').addEventListener('click', function (event) {
     event.preventDefault()
 
-    const inputValue = document.getElementById('input-value').value
-    console.log(inputValue)
+    const phoneNumber = document.getElementById('input-value').value
+    const pinNumber = document.getElementById('pin-number').value
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        window.location.href = './home.html'
+    }
+    else {
+        alert("Incorrect Phone or Pin")
+    }
 
 })
